b44ba7fb34dc6c18c656c41a39957b60679ec153
fedemelo <f.melo@uniandes.edu.co>
2024-09-15 19:54:45 -0500

    Update app.module.ts with new modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,30 @@
-import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppService } from './app.service';
+import { Module } from '@nestjs/common';
+
+import { ProductStoreModule } from './product-store/product-store.module';
+import { ProductModule } from './product/product.module';
+import { StoreModule } from './store/store.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'parcial_practico_apis',
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      dropSchema: true,
+      synchronize: true,
+      keepConnectionAlive: true,
+    }),
+    ProductModule,
+    StoreModule,
+    ProductStoreModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
